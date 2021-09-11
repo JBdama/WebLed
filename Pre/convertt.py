d8da5d44dbc628389be2fd0f54b6e3eec61e0982
Julius <juliusbalzer22@gmail.com>
2021-09-12 00:37:15 +0200

    PC

--- Pre/convertt.py
+++ Pre/convertt.py
@@ -1,44 +1,47 @@
 import os
 import codecs
 css_list =[ "colorpicker.css",  "nav.css", "slider.css", "main.css"]         # List with all CSS filenames
-def readjs():
-    with codecs.open(os.path.join("After", "logic.js"), "r") as f:
-        js = f.read()
-        f.close()
-        return js
 
-def readhtml():
-    with codecs.open(os.path.join("After", "index.html"), "r", encoding = 'utf-8') as f:
-        html = f.read()
+def read_file(path, file):
+    with codecs.open(os.path.join(path, file), "r", encoding="utf-8") as f:
+        content = f.read()
         f.close()
-        return html
+        return content
 
+def read_css(path, files):
+    css = ""
+    for c in files:
+        content = read_file(path, c)
+        for line in content:
+            new = line.rstrip()
+            css += new
+        css += "\n"
+    print(css)
 def readcss():
     css = ""
-    css_stor = []
     for css_file in css_list:
         with codecs.open(os.path.join("After", css_file), "r", encoding = 'utf-8') as f:
-            css = f.read()
-            css_stor.append(css)
+            css_temp = f.read()
+            css_temp = css_temp.splitlines()
+            css += "".join(css_temp)
+            css += "\n"
+            #css_stor.append(css)
             f.close()
-    return css_stor
+    return css
+
 def edithtml(html):
-    print(type(html))
+    #print(type(html))
     html_list = html.splitlines()
     html_copy = html_list.copy()
-    for i in range(len(html_list)-1, -1, -1):
-        if html_list[i] == "</body>":
-            print(i, html_list[i])
-            html_copy.insert(i, "JAVSCRIPT")
-            break
+
     for i, line in enumerate(html_list):
         if line == "<head>":
-            print(i)
+            #print(i)
             css_count = len(css_list)
             half_one = html_copy[:i+1]
             half_two = html_copy[i+1+css_count:]
-            half_one.append("<style>")
-            half_two.insert(0, "</style>")
+            half_two.pop()
+
             string1 = "".join(half_one)
             string2 = "".join(half_two)
             #(string1, "\n")
@@ -47,51 +50,50 @@ def edithtml(html):
             break
     
     #print(html_copy)
- 
-def hans(html):
-    html2 = html.splitlines()
-    for i in range(len(html2)-1,-1,-1):
-        print(html2[i])
-        if html2[i] == "</body>":
-            print(i)
-    return html
 
 
-def writehtml(html1, html2, css_stora, js):
+def writehtml(html1, html2, css, js):
     with open(os.path.join("NodeMCU/src", "text.txt"), "w") as f:
-        #html = str(html).replace('\n', '\\n')
         js = str(js).replace('"', "'")
         js.strip()
         js = js.splitlines()
         html1 = str(html1).replace('"', "'")
         html1 = html1.splitlines()
         html2 = str(html2).replace('"', "'")
+
         html2 = html2.splitlines()
-        f.write("<script>")
-        for line in js:
-            f.write(line)
-        f.write('</script>')
         for line in html1:
             f.write(line)
+        f.write('\n  <style>\n')
+        f.write(css)
+        f.write('  </style>\n')
+
         for line in html2:
             f.write(line)
-        for css in css_stora:
-            css = str(css).replace('"', "'")
-            css = css.splitlines()
-            f.write('"')
-            for c in css:
-                f.write(c)
-            f.write('\\n "\n')
+        f.write('\n  <script> \n ')
+        for line in js:
+            f.write(line)
+        f.write('\n  </script>')
+        f.write('\n</html>')
         f.close()
 
-#print(os.getcwd())
-js = readjs()
-html = readhtml()
-#html = hans(html)
-html1, html2 = edithtml(html)
-htnl1, html2 = "", ""
-css = readcss()
-html = " "
-test = ""
-css =  ""
-writehtml(html1, html2, css, js)
+def calculate():
+    #print(os.getcwd())
+    js = read_file("After", "logic.js")
+    html = read_file("After", "index.html")
+    css = read_css("After", css_list)
+    html1, html2 = edithtml(html)
+    html = " "
+    test = ""
+    #css =  ""
+    writehtml(html1, html2, css, js)
+#calculate()
+
+css = ""
+
+
+#css = css.splitlines()
+#css = "".join(css)
+#print(css)
+with open(os.path.join("Pre", "help.txt"), "w") as f:
+    f.writelines(css)
